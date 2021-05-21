7242c1c298e40be1735471be873dcb3e10c0427f
George Ballard <ballard.george@gmail.com>
2021-05-21 16:41:43 -0700

    missed frontend updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Generator from "./components/Generator.js"
+import {useState} from 'react'
 
 export default App => {
+  const [state, setState] = useState("")
   return (
     <div className="App">
-    <h2>Typing Evaluator!</h2>
-    <h3>Click the button to randomly generate a paragraph to type!</h3>
-        <Generator className="btn"/>
+      <h2>Typing Evaluator!</h2>
+      <h3>Click the button to randomly generate a paragraph to type!</h3>
+      <Generator state={state} setState={setState} />
+      <p>The current state is: {state}</p>
     </div>
   );
 }
--- frontend/src/components/Generator.js
+++ frontend/src/components/Generator.js
@@ -1,14 +1,19 @@
 // Button component that takes in props
 import axios from 'axios'
 
-export default Generator => {
+const Generator = props => {
     const clickHandler = () => {
         console.log("testing")
         axios.get('http://localhost:5000/api/test')
-        .then(res => console.log(res))
+        .then(res => {
+            console.log(res)
+            props.setState(res.data)
+        })
         .catch(err => console.log(err))
     }
     return (
         <button onClick={ () => clickHandler() }>Generate!</button>
     )
 }
+
+export default Generator
