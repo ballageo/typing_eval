2308cded7ac8599e5c406804009e5252f1dbd9c9
George Ballard <ballard.george@gmail.com>
2021-05-24 16:34:05 -0700

    conditionally renders input when data generated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import Generator from "./components/Generator.js"
+import {Generator} from "./components/Generator.js"
+import {Input} from "./components/Input.js"
 import {useState} from 'react'
 
 const App = () => {
   const [state, setState] = useState("")
+  let input;
+  if (state === "") {
+    input = null
+  } else {
+    input = <Input />
+  }
   return (
     <div className="App">
       <h2>Typing Evaluator!</h2>
       <h3>Click the button to randomly generate a paragraph to type!</h3>
       <Generator state={state} setState={setState} />
+      {input}
       <p>The current state is: {state}</p>
     </div>
   );
---
+++ frontend/src/components/Input.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export const Input = props => {
+    return (
+        <textarea rows="10" cols="100"></textarea>
+    )
+}
