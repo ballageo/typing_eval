f35ee313206021e9e5c1a3c16d470962b095d938
Jin Kang <kjinn62@gmail.com>
2021-05-24 09:33:48 -0700

    fixed possible merge conflicts

--- backend/typing_eval_app/__init__.py
+++ backend/typing_eval_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 CORS(app)
 
--- backend/typing_eval_app/controllers/typing_eval_backend.py
+++ backend/typing_eval_app/controllers/typing_eval_backend.py
@@ -1,6 +1,8 @@
 from typing_eval_app import app
 from flask import jsonify
+from essential_generators import DocumentGenerator
 
+gen = DocumentGenerator() # provides ability to generate random data
 
 @app.route('/')
 def index():
@@ -17,3 +19,7 @@ jin = User('jin', 25)
 @app.route('/api/test')
 def api_index():
     return jsonify([george.__dict__, jin.__dict__])
+
+@app.route('/api/generate')
+def doc_generate():
+    return jsonify(gen.paragraph()) # generates a new random paragraph of content and returns a response object in JSON
