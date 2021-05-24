49214e990aee795c17588ecc8de2ae2b423e8b87
George Ballard <ballard.george@gmail.com>
2021-05-24 14:49:48 -0700

    random-word package seems slow, will try API-based generation

--- backend/typing_eval_app/controllers/typing_eval_backend.py
+++ backend/typing_eval_app/controllers/typing_eval_backend.py
@@ -1,8 +1,6 @@
 from typing_eval_app import app
 from flask import jsonify
-from essential_generators import DocumentGenerator
-
-gen = DocumentGenerator() # provides ability to generate random data
+from random_word import RandomWords
 
 @app.route('/')
 def index():
@@ -22,4 +20,9 @@ def api_index():
 
 @app.route('/api/generate')
 def doc_generate():
-    return jsonify(gen.paragraph()) # generates a new random paragraph of content and returns a response object in JSON
+    r = RandomWords()
+    WPM_TEXT = r.get_random_words(minLength=3, maxLength=6, minDictionaryCount=5, limit=400)
+    words = ' '.join(word for word in WPM_TEXT)
+    print(words)
+    print(len(words))
+    return jsonify(words) # generates a new random paragraph of content and returns a response object in JSON
