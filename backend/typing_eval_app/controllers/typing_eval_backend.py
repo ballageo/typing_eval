c7eb50d60e572057f274289a6f108037eb0e0401
George Ballard <ballard.george@gmail.com>
2021-05-26 17:28:00 -0700

    successfully post data to backend

--- backend/typing_eval_app/controllers/typing_eval_backend.py
+++ backend/typing_eval_app/controllers/typing_eval_backend.py
@@ -5,32 +5,37 @@ from typing_eval_app import app
 from flask import jsonify, request
 import requests, random
 import json
+from flask_cors import CORS, cross_origin
+
+CORS(app)
 
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 
-@app.route('/')
+@app.get('/')
 def index():
     return "STRING"
 
-@app.route('/api/generate')
+@app.get('/api/generate')
 def doc_generate():
     global WORDS
     res = ' '.join(word.decode() for word in random.choices(WORDS, k=250))
     return jsonify(res) # generates a new random paragraph of content and returns a response object in JSON
 
-@app.route('/user/get/<int:id>')
+@app.get('/api/user/get/<int:id>')
 def show_user(id):
     user = User.get_one({"id":id})
     return jsonify(user.__dict__)
 
-@app.route('/stat/create', methods = ['post'])
+@cross_origin
+@app.post('/api/stat/create')
 def create_stat():
-    data = {
-        "user_id" : 'placehold', #USER ID HERE
-        "wpm" : 100, #WPM HERE
-        "accuracy" : 99.9 #ACCURACY HERE
-    }
-    Stat.save(data)
-    return redirect('http://localhost:3000')
+    print(request.get_json())
+    # data = {
+    #     "user_id" : 'placehold', #USER ID HERE
+    #     "wpm" : 100, #WPM HERE
+    #     "accuracy" : 99.9 #ACCURACY HERE
+    # }
+    # Stat.save(data)
+    return jsonify("testing")
